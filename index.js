bd32cc2d849bc0e6ae135b78f85425eb81f49419
Lucky Dewangan <luckydewangan791@gmail.com>
2024-02-21 21:17:02 +0530

    removed jquery logic and converted to plain js

--- index.js
+++ index.js
@@ -7,40 +7,41 @@ var words = ['Predictions powered by the crowd...', 'Harness the wisdom of top f
     skip_count = 0,
     skip_delay = 15,
     speed = 70;
+
 var wordflick = function () {
-  setInterval(function () {
-    if (forwards) {
-      if (offset >= words[i].length) {
-        ++skip_count;
-        if (skip_count == skip_delay) {
-          forwards = false;
-          skip_count = 0;
+    setInterval(function () {
+        if (forwards) {
+            if (offset >= words[i].length) {
+                ++skip_count;
+                if (skip_count == skip_delay) {
+                    forwards = false;
+                    skip_count = 0;
+                }
+            }
         }
-      }
-    }
-    else {
-      if (offset == 0) {
-        forwards = true;
-        i++;
-        offset = 0;
-        if (i >= len) {
-          i = 0;
+        else {
+            if (offset == 0) {
+                forwards = true;
+                i++;
+                offset = 0;
+                if (i >= len) {
+                    i = 0;
+                }
+            }
         }
-      }
-    }
-    part = words[i].substr(0, offset);
-    if (skip_count == 0) {
-      if (forwards) {
-        offset++;
-      }
-      else {
-        offset--;
-      }
-    }
-    $('.word').text(part);
-  },speed);
+        part = words[i].substr(0, offset);
+        if (skip_count == 0) {
+            if (forwards) {
+                offset++;
+            }
+            else {
+                offset--;
+            }
+        }
+        document.querySelector('.word').textContent = part;
+    }, speed);
 };
 
-$(document).ready(function () {
-  wordflick();
+document.addEventListener('DOMContentLoaded', function () {
+    wordflick();
 });
