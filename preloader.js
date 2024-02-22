ff6847e76de3d99f4518060612208e7edf60f5fd
Lucky Dewangan <luckydewangan791@gmail.com>
2024-02-22 20:10:44 +0530

    modified code

--- preloader.js
+++ preloader.js
@@ -149,14 +149,13 @@
      }, speed);
  };
  
-
+// Start word flicker
+wordflick();
 
 // Function to hide the preloader
 function Preloader() {
     var parentDiv = document.querySelector('.parent');
     parentDiv.style.display = 'flex';
-    // Start word flicker
-    wordflick();
 }
 
 // Hide the preloader when the DOM content is loaded
